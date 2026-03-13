7ddb3d994ffa72f3d5a656de7903dfd82575f6a6
unknown <monirhrabby.personal@gmail.com>
2026-03-13 17:13:26 +0600

    cache

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async headers() {
+    return [
+      {
+        source:
+          "/:all*(svg|jpg|jpeg|png|gif|webp|avif|ico|css|js|woff|woff2|ttf)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
